925b17a4b5f45965a06c3c730574fe9d51baa577
alfian-f <alfian.0615@gmail.com>
2024-03-16 21:50:41 +0700

    Initial Commit

---
+++ src/main/java/id/ac/ui/cs/advprog/adproggameshop/AdprogGameshopApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.adproggameshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdprogGameshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdprogGameshopApplication.class, args);
+    }
+
+}
