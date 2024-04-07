11cc4f99e13eea1fe12fc1a645e7d19e60d6cd4d
alfian-f <alfian.0615@gmail.com>
2024-04-07 15:41:27 +0700

    Created Transaction Services

--- src/main/java/id/ac/ui/cs/advprog/adproggameshop/repository/TransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/adproggameshop/repository/TransactionRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findAllByUser(User user);
+    List<Transaction> findAllByBuyerOrSeller(User buyer, User seller);
 }
