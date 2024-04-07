14d783a5a040a438f604d833ff4b26b9de8a87be
Sirered <gikurniawan.1995@gmail.com>
2024-04-07 11:51:07 +0700

    Adding more Enums

--- src/main/java/id/ac/ui/cs/advprog/adproggameshop/enums/CategoryEnums.java
+++ src/main/java/id/ac/ui/cs/advprog/adproggameshop/enums/CategoryEnums.java
@@ -2,7 +2,13 @@ package id.ac.ui.cs.advprog.adproggameshop.enums;
 
 public enum CategoryEnums {
     TOY("Toy"),
-    BOARDGAME("Board Game");
+    BOARDGAME("Board Game"),
+    SWITCH("Switch Game"),
+    XBOXSERIESX("Xbox Series X Game"),
+    XBOXONE("Xbox One Game"),
+    PS5("PS5 Game"),
+    PS4("PS4 Game");
+
 
     private String label;
 
