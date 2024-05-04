114eead7ad4b1214a0d3cfefe3f294e73aac1122
TomoriNow <seanarcher1974@gmail.com>
2024-05-04 19:06:04 +0700

    Implemented tests for login, logout, register, editprofile, profile page, etc.

--- src/test/java/model/UserTest.java
+++ src/test/java/model/UserTest.java
@@ -27,4 +27,20 @@ public class UserTest {
         assertArrayEquals(profilePicture, user.getProfilePicture());
     }
 
+    @Test
+    public void testEqualsAndHashCode() {
+        User user1 = new User("username", "email@example.com", "password");
+        User user2 = new User("username", "email@example.com", "password");
+
+        assertEquals(user1, user2);
+        assertEquals(user1.hashCode(), user2.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        User user = new User("username", "email@example.com", "password");
+
+        assertEquals("User{userId='null', username='username', email='email@example.com', balance=0.0, bio='null', profilePicture=null}", user.toString());
+    }
+
 }
