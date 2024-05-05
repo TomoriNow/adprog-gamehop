6ae680eb2ae0d4dd52731fe1556e9356cbeb1da0
alfian-f <alfian.0615@gmail.com>
2024-05-05 20:51:12 +0700

    Added test for @Setter in transaction model

--- src/test/java/model/TransactionTest.java
+++ src/test/java/model/TransactionTest.java
@@ -1,6 +1,8 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import id.ac.ui.cs.advprog.adproggameshop.model.Transaction;
@@ -9,7 +11,58 @@ import id.ac.ui.cs.advprog.adproggameshop.model.User;
 import java.util.Date;
 
 public class TransactionTest {
+    private Transaction transaction;
+    private User buyer;
+    private User seller;
+    private Date initialDate;
 
+    @BeforeEach
+    public void setUp() {
+        buyer = new User();
+        seller = new User();
+        initialDate = new Date();
+        transaction = new Transaction();
+    }
+
+    @Test
+    public void testSetBuyer() {
+        transaction.setBuyer(buyer);
+        assertEquals(buyer, transaction.getBuyer());
+    }
+
+    @Test
+    public void testSetSeller() {
+        transaction.setSeller(seller);
+        assertEquals(seller, transaction.getSeller());
+    }
+
+    @Test
+    public void testSetProductName() {
+        String productName = "GameController";
+        transaction.setProductName(productName);
+        assertEquals(productName, transaction.getProductName());
+    }
+
+    @Test
+    public void testSetDate() {
+        Date newDate = new Date();
+        transaction.setDate(newDate);
+        assertEquals(newDate, transaction.getDate());
+    }
+
+    @Test
+    public void testSetAmount() {
+        int amount = 5;
+        transaction.setAmount(amount);
+        assertEquals(amount, transaction.getAmount());
+    }
+
+    @Test
+    public void testSetTotal() {
+        double total = 499.95;
+        transaction.setTotal(total);
+        assertEquals(total, transaction.getTotal());
+    }
     @Test
     public void testTransactionConstructor() {
         User buyer = new User();
