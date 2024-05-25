b1d85b7f83b5d0ade86c050a2c30a8945b6c9d98
alfian-f <alfian.0615@gmail.com>
2024-05-25 17:04:19 +0700

    Added item amount in transaction history

--- src/main/java/id/ac/ui/cs/advprog/adproggameshop/service/TransactionService.java
+++ src/main/java/id/ac/ui/cs/advprog/adproggameshop/service/TransactionService.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.adproggameshop.service;
 
-import id.ac.ui.cs.advprog.adproggameshop.model.Transaction;
 import id.ac.ui.cs.advprog.adproggameshop.model.User;
 import id.ac.ui.cs.advprog.adproggameshop.utility.TransactionDTO;
 
