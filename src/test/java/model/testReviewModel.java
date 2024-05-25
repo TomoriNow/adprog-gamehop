a43fd6fe3ced12de76dad4f89666a2c7c8156cd0
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-05-25 16:38:11 +0700

    Renaming review model class name

--- src/test/java/model/testReviewModel.java
+++ src/test/java/model/testReviewModel.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ReviewTest {
+public class testReviewModel {
 
     @Test
     public void testReviewModel() {
