4b75a102e5821daac55c1285d9ebdf5b5c1d0257
alfian-f <alfian.0615@gmail.com>
2024-05-26 20:31:52 +0700

    removed unused import in transaction service

--- src/main/java/id/ac/ui/cs/advprog/adproggameshop/service/TransactionServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/adproggameshop/service/TransactionServiceImpl.java
@@ -1,9 +1,7 @@
 package id.ac.ui.cs.advprog.adproggameshop.service;
 
-import id.ac.ui.cs.advprog.adproggameshop.model.Transaction;
 import id.ac.ui.cs.advprog.adproggameshop.model.User;
 import id.ac.ui.cs.advprog.adproggameshop.repository.TransactionRepository;
-import id.ac.ui.cs.advprog.adproggameshop.repository.UserRepository;
 import id.ac.ui.cs.advprog.adproggameshop.utility.TransactionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
